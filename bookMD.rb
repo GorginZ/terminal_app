a3f2d95e4dd11baa359869f625d84c6d589b9396
GorginZ <georgia.z.leng@gmail.com>
2020-03-12 12:32:27 +1100

    initial commit

---
+++ bookMD.rb
@@ -0,0 +1,34 @@
+
+    puts "Welcome to bookMD"
+    puts "If you are experiencing a medical emergency call 000"
+    puts "1. Book Appointment"
+    puts "2. Manage appointments"
+    puts "if you need help type --help"
+    puts "  >"
+
+def menu_select
+
+user_input = gets.chomp
+
+  case
+  when user_input == "1"
+puts "book appointment"
+  when user_input == "2"
+    puts "your appointments..."
+  end
+end
+
+
+p loop {menu_select}
+
+# def user_selector
+#     user_input = gets.chomp
+#     input.array = [1,2,3]
+#     case
+#     when user_input == integer
+#         user_input = getschomp.to_i
+#     end
+#     when userinput.is string
+#         user_input.hash {1, "book", "Book", ;2;3;4;5;6;7}
+# end
+    
---
+++ user.rb
@@ -0,0 +1,57 @@
+# class Patients
+#     attr_accessor :name, :dob
+#     def initialize (name,dob)
+#         @name = name
+#         @dob = dob
+#       end
+#     end       
+
+
+patient1 = {
+  'Name': 'Jack',
+  'Phone': '000000',
+  'Email': 'Jack@gmail.com'   #hash
+}
+patients = [patient1]      #this contact1 hash gets stored in an array 
+
+
+def make_patient()
+print "Name: "
+    name = gets.chomp
+    print "Phone: "
+    phone = gets.chomp
+    print "Email: "
+    email = gets.chomp
+    patient = {
+      'Name': name,
+      'Phone': phone,
+      'Email': email
+    }
+    patient.push(patients)
+    
+end    
+
+make_patient
+
+
+# patients.each do |key, value|   #access all patients
+#     puts key
+# end    
+
+
+# all_patients = {}
+# # all_patients << 
+# new_patient = Patients.new "georgia","10/03/1994"
+# all_patients < new_patient
+# p new_patient
+
+# p all_patients
+
+#   def make_patients
+#     puts "full name"
+#   new_patient = gets.chomp
+#   puts "dob dd/mm/yyyy"
+#   @dob = gets.chomp
+
+
+#   end   
