24d0f0932c28d5e6a7ac1533ee62685cee27e2a9
GorginZ <georgia.z.leng@gmail.com>
2020-03-14 18:56:47 +1100

    avail chart

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
-gem 'tty-prompt'
+gem "tty-prompt"
+gem "tty-table"
 
 gem "colorize", "~> 0.8.1"
--- bookMD.rb
+++ bookMD.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'tty-prompt'
 require 'tty-reader'
+require 'tty-table'
+
+def booker
+    puts "availability for all doctors"
+    availability = TTY::Table.new ['Monday 16','Tuesday 17','Wednesday 18','Thursday 19','friday 20'], [['8.00am', '8.00am','--','--','8.00am',], ['8.30am', '8.30am','8.30am','8.30am','8.30am'], ['9.00am', '9.00pm','9.00am','9.00am','9.00am'],['9.30am', '9.30am','9.30am','9.30am','9.30am'],['10.00am', '10.00am','10.00am','10.00am','10.00am'],['10.30am', '10.30am','10.30am','10.30am','10.30am']]
+    #each new array entry is new row, first array is collumns
+
+availability.render(:ascii)
+
+end   
 
 
 def start 
@@ -12,7 +22,7 @@ def start
     end
     
     if welcome == 'book_appointment'
-        puts "availabilities"
+        print booker  # availability/booker method will go here
     elsif welcome == 'manage_appointments'
         puts "current appointments"
     end
@@ -23,9 +33,4 @@ start
 
 
 
-def booker
-
-
-
-end   
 
