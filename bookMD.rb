92b85382976eedfee87967cd86b262014a0c9863
GorginZ <georgia.z.leng@gmail.com>
2020-03-14 16:23:12 +1100

    finally home menu passing on returned value(user selection)

--- bookMD.rb
+++ bookMD.rb
@@ -1,42 +1,31 @@
-
+require 'rubygems'
 require 'tty-prompt'
-
-home_menu = TTY::Prompt.new
-home_menu.select("What do you need today?", %w(Book_appointment Manage_appointments))
+require 'tty-reader'
 
 
+def start 
+    prompt = TTY::Prompt.new
+    system("clear")
+    welcome = prompt.select("welcome to bookMD") do |menu|
+        menu.choice 'book_appointment'
+        menu.choice 'manage_appointments'
+    end
     
-    
-    # puts "Welcome to bookMD"
-    # puts "If you are experiencing a medical emergency call 000"
-    # puts "1. Book Appointment"
-    # puts "2. Manage appointments"
-    # puts "if you need help type --help"
-    # puts "  >"
+    if welcome == 'book_appointment'
+        puts "availabilities"
+    elsif welcome == 'manage_appointments'
+        puts "current appointments"
+    end
+end   
 
-def menu_select
-
-user_input = gets.chomp
-
-  case
-  when user_input == "1"
-puts "book appointment"
-  when user_input == "2"
-    puts "your appointments..."
-  end
-end
+start
 
 
-p loop {menu_select}
 
-# def user_selector
-#     user_input = gets.chomp
-#     input.array = [1,2,3]
-#     case
-#     when user_input == integer
-#         user_input = getschomp.to_i
-#     end
-#     when userinput.is string
-#         user_input.hash {1, "book", "Book", ;2;3;4;5;6;7}
-# end
-    
+
+def booker
+
+
+
+end   
+
